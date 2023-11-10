f1e1b2df1f4b664c2525da476c2c5c2a6b968dca
GooseSzk <143315440+GooseSzk@users.noreply.github.com>
2023-11-11 01:43:29 +0500

    Add files via upload

---
+++ dom kotunov/script/data.js
@@ -0,0 +1,25 @@
+let info = [
+    {
+        img:'https://images.drive.ru/i/0/5d75ea9fec05c4a22a000015.jpg',
+        name:'Bugatti Chiron Super Sport 300+',
+        text:'Этот автомобиль установил рекорд скорости для серийных автомобилей, достигнув скорости 304,77 миль в час (490,48 км/ч).'
+    } 
+    ,
+    {
+        img:'https://avatars.mds.yandex.net/get-vertis-journal/4471904/unnamed-2.jpg_1691564462674/orig',
+        name:'Hennessey Venom F5',
+        text:'Ожидается, что этот автомобиль достигнет скорости более 300 миль в час (482 км/ч), но его реальная скорость еще не была подтверждена.'
+    }
+    ,
+    {
+        img:'https://cdn.motor1.com/images/mgl/erZ6K/s1/koenigsegg-jesko-absolut.jpg',
+        name:'Koenigsegg Jesko Absolut',
+        text:'Этот автомобиль разрабатывался с целью достижения максимальной скорости, и ожидается, что он сможет превысить 300 миль в час (482 км/ч).'
+    }
+    ,
+    {
+        img:'https://autofog.ru/img/bugatti/veyron-super-sport/bugatti-veyron-super-sport.jpg',
+        name:'Bugatti Veyron Super Sport',
+        text:'Этот автомобиль имеет максимальную скорость около 268 миль в час (431 км/ч).'
+    }
+]
